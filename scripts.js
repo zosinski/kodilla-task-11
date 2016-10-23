c55f4a09755e6448774a45f3ae310c14e8aad17d
Zbigniew Osiński <Zbigniew Osiski>
2016-10-23 16:43:00 +0200

    initial commit

--- scripts.js
+++ scripts.js
@@ -1,19 +1,27 @@
 var url = 'http://api.icndb.com/jokes/random';
-var button = document.getElementById('btn-get-joke');
-var paragraph = document.getElementById('joke');
+var button = $('#btn-get-joke');
+var paragraph = $('#joke');
 
 getJoke();
 
-button.addEventListener('click', function(){
+button.click(function(){
 	getJoke();
 });
 
 function getJoke() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url);
-	xhr.addEventListener('load', function(){
-		var response = JSON.parse(xhr.response);
-		paragraph.innerText = response.value.joke;
+	$.ajax({
+		method: 'GET',
+		url: url,
+		success: function (res) {
+			paragraph.text(res.value.joke);
+		}
 	});
-	xhr.send();
+
+	// var xhr = new XMLHttpRequest();
+	// xhr.open('GET', url);
+	// xhr.addEventListener('load', function(){
+	// 	var response = JSON.parse(xhr.response);
+	// 	paragraph.innerText = response.value.joke;
+	// });
+	// xhr.send();
 }
