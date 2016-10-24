caa00063bc7e2202f9fb4e8e638abf966d154962
Zbigniew Osiński <Zbigniew Osiski>
2016-10-24 02:36:19 +0200

    initial commit

--- scripts.js
+++ scripts.js
@@ -1,19 +1,59 @@
-var url = 'http://api.icndb.com/jokes/random';
-var button = document.getElementById('btn-get-joke');
-var paragraph = document.getElementById('joke');
 
-getJoke();
+$('#search').click(searchCountries);
+$('#country-name').keydown( function(event) {
+	if (event.which == 13) {searchCountries()};
+})
 
-button.addEventListener('click', function(){
-	getJoke();
-});
+function searchCountries() {
+	var url = 'https://restcountries.eu/rest/v1/name/';
+ 	var countryName = $('#country-name').val();
+	if (!countryName.length) countryName = 'Poland';
 
-function getJoke() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url);
-	xhr.addEventListener('load', function(){
-		var response = JSON.parse(xhr.response);
-		paragraph.innerText = response.value.joke;
+	var request = $.ajax({
+		url: url + countryName,
+		method: 'GET',
+		success: showCountriesList,
+		error: hideCountriesList
 	});
-	xhr.send();
+
+}
+
+function showCountriesList(res) {
+	var countriesList = $('#countries-list');
+	var countryTable = $('.country-table').last().clone();
+	var countriesListHeader = $('#countries-list-header');
+	countriesList.empty();
+	countriesList.css('display','flex');
+
+	var countryName = $('#country-name').val() ? $('#country-name').val() : 'Poland';
+	countriesListHeader.text('Search results matching \"' + countryName +'\"').css('display', 'block');
+
+	res.forEach(function(item){
+		
+		// uzupełnianie danych tabeli
+		countryTable.find('.country-name').text(item.name);
+		countryTable.find('.capital').text(item.capital);
+		countryTable.find('.land-area').text(item.area);
+		countryTable.find('.population').text(item.population);
+		countryTable.find('.currency').text(item.currencies.join(", "));
+		countryTable.find('.languages').text(item.languages.join(", ")); 
+
+		// dodanie tabeli do DOM
+		countriesList.append( countryTable );
+		
+		// skopiowanie szablonu tabeli
+		countryTable = $('.country-table').last().clone();
+
+	});
+}
+
+function hideCountriesList(jqXHR) {
+	$('#countries-list').css('display', 'none');
+	$('#countries-list-header').css('display', 'none');
+
+	if (jqXHR.status === 404) {
+		var alert = $('#search-alert').addClass('alert alert-danger').text('No match. Try again.');
+		alert.show('fast');
+		setTimeout( function() {alert.hide('slow')}, 3000 ); 	
+	}
 }
